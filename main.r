3e1db21c4a48dd6da8d6837a6bfd74b7ce350c3c
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2020-11-18 10:29:29 -0300

    Classify datasets with type, name and chip used

--- main.r
+++ main.r
@@ -7,25 +7,52 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 #BiocManager::install("pathifier")
 #library(pathifier)
 
-source("nsga2.r")
-source("matrices_evaluation")
+#source("nsga2.r")
+source("matrices_evaluation.r")
+source("data_sampling.r")
 
-samplesDir = "data/training/samples/"
-testFile = paste(samplesDir, "Leukemia_GSE28497-29-col-sample-10-percent.csv", sep="")
-data <- read.csv(testFile, header = TRUE, sep = ",", quote= "", dec = ".", row.names = 1)
+testFile = paste(sampleDir, "/Leukemia_GSE28497-3-col-sample-5-percent.csv", sep="")
+data <- readFile(testFile)
 
-chips = list(
-  Leukemia = "[HG-U133A] Affymetrix Human Genome U133A Array", #affy_hg_u133a
-  Liver = "[HT_HG-U133A] Affymetrix HT Human Genome U133A Array",
-  Renal = "[HG-U133_Plus_2] Affymetrix Human Genome U133 Plus 2.0 Array", #affy_hg_u133_plus_2
-  Breast = "Agilent-028004 SurePrint G3 Human GE 8x60K Microarray", #agilent_sureprint_g3_ge_8x60k_v2
-  Colorectal = "[HG-U219] Affymetrix Human Genome U219 Array",
-  Lung = "[HG-U133_Plus_2] Affymetrix Human Genome U133 Plus 2.0 Array", #affy_hg_u133_plus_2
-  Prostate = "[HG-U133_Plus_2] Affymetrix Human Genome U133 Plus 2.0 Array" #affy_hg_u133_plus_2
+datasets = list(
+  Leukemia = list(
+    name = "Leukemia_GSE28497",
+    chip = "GPL96",
+    type = "training"
+  ),
+  Liver = list(
+    name = "Liver_GSE14520_U133A",
+    chip = "GPL3921",
+    type = "training"
+  ),
+  Renal = list(
+    name = "Renal_GSE53757",
+    chip = "GPL570",
+    type = "training"
+  ),
+  Breast = list(
+    name = "Breast_GSE70947",
+    chip = "GPL13607",
+    type = "evaluation"
+  ),
+  Colorectal = list(
+    name = "Colorectal_GSE44076",
+    chip = "GPL13667",
+    type = "evaluation"
+  ),
+  Lung = list(
+    name = "Lung_GSE19804",
+    chip = "GPL570",
+    type = "evaluation"
+  ),
+  Prostate = list(
+    name = "Prostate_GSE46602",
+    chip = "GPL570",
+    type = "evaluation"
+  ),
 )
 
-data <- clean.data.without.entrez.id(data)
-colnames(data) <- gene.id.translate(colnames(data), 'affy_hg_u133a')
+data <- clean.and.translate.entrez.id(data, "GPL96")
 
-expressionMatrix = expression.matrix.pearson(t(testFile))
-biologicalMatrix = biological.matrix.GO(colnames(testFile))
+expressionMatrix = expression.matrix(t(data))
+biologicalMatrix = biological.matrix.go(colnames(data))

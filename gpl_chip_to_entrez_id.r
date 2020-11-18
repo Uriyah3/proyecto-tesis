b280deadcd39ab0e6ca8dd9939d655b172ad8553
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2020-11-18 10:28:52 -0300

    Add chips used in downloaded datasets and comments functions used to read them

--- gpl_chip_to_entrez_id.r
+++ gpl_chip_to_entrez_id.r
@@ -1,13 +1,31 @@
-
+#' Reads a GPL data file using read.csv
+#' Note: The GPL files where downloaded from CuMiDa
+#' 
+#' @param filename The path to the file
+#' @return A matrix with the information stored in \code{filename}
+#' @examples
+#' gpl_data = read.gpl("data/chip_to_entrez_id/GPL96-57554.txt")
+#' 
 read.gpl <- function(filename) {
-  gpl = read.csv(filename, header = TRUE, sep = "\t", quote= "", dec = ".", row.names = 1)
+  gpl = read.csv(filename, header = TRUE, sep = "\t", quote= "", dec = ".", row.names = 1, check.names = FALSE)
 }
 
-GPL_CHIPS <- list(
-  gpl96 = "data/chip_to_entrez_id/GPL96-57554.txt"
-  
+# Convert the respective gpl tool to the file that stores its data
+gpl_to_file <- list(
+  GPL96 = "data/chip_to_entrez_id/GPL96_limpo.txt",
+  GPL3921 = "data/chip_to_entrez_id/GPL3921_limpo.txt",
+  GPL570 = "data/chip_to_entrez_id/GPL570_limpo.txt",
+  GPL13607 = "data/chip_to_entrez_id/GPL13607_limpo.txt",
+  GPL13667 = "data/chip_to_entrez_id/GPL13667_limpo.txt"
 )
 
+#' Returns the ENTREZ_GENE_ID for all genes in the GPL tool
+#' 
+#' @param chip Code of the GPL chip used in the experiment
+#' @return A matrix with gene to ENTREZ_GENE_ID info for the GPL \code{chip}
+#' @examples
+#' gene_to_id = process.gpl("GPL96")
+#'
 process.gpl <- function(chip) {
   gpl_data = read.gpl(GPL_CHIPS[[chip]])
   gpl_data = gpl_data[ ,"ENTREZ_GENE_ID", drop=FALSE]

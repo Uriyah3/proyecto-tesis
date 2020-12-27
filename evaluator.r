1d02b7bda99c02e35f6207e40e6f08cd098b4b9b
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2020-12-27 18:49:34 -0300

    Calculate silhouette and hypervolume indices

--- evaluator.r
+++ evaluator.r
@@ -1,9 +1,38 @@
 library("amap")
+library("mco")
+library("cluster")
 source("nsga2.r")
 
+helper.normalize <- function(data) {
+  return( (data - min(data)) / (max(data) - min(data)) )
+}
 
-evaluator.multiobjective.clustering <- function(solutions) {
-  return( solutions )
+evaluator.multiobjective.clustering <- function( results, dmatrix ) {
+  
+  num_clusters <- ncol(results$population) - 4
+  solution_count <- length(results$clustering)
+  
+  silhouette_indices = double(solution_count)
+  for (c_index in 1:solution_count ) {
+    clustering <- results$clustering[[c_index]]
+    
+    sil <- silhouette( clustering, dmatrix = as.matrix(dmatrix) )
+    silhouette_indices[c_index] <- summary(sil)$avg.width
+  }
+  
+  pareto_points <- as.matrix( results$population[ , (num_clusters+1):(num_clusters+2) ] )
+  hypervolume <- dominatedHypervolume(pareto_points)
+  
+  pareto_points[, 1] <- helper.normalize(pareto_points[, 1])
+  pareto_points[, 2] <- helper.normalize(pareto_points[, 2])
+  n_hypervolume <- dominatedHypervolume(pareto_points, c(1, 1))
+  
+  metrics <- list(
+    silhoutte = silhouette_indices,
+    hypervolume = hypervolume,
+    normalized_hypervolume = n_hypervolume
+  )
+  return( metrics )
 }
 
 evaluator.biological.significance <- function() {

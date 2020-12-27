4b4b9e7f23a5bfc67081d62d852daaf6761d8940
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2020-12-27 18:51:48 -0300

    Change some variable names and add new packages used

--- main.r
+++ main.r
@@ -1,18 +1,29 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-#BiocManager::install("org.Hs.eg.db")
 #BiocManager::install("GOSemSim")
-#BiocManager::install("biomaRt")
-#BiocManager::install("pathifier")
-#library(pathifier)
+#BiocManager::install("DOSE")
+#X BiocManager::install("biomaRt")
+#X BiocManager::install("pathifier")
+#BiocManager::install("meshes")
+#BiocManager::install("MeSH.Hsa.eg.db")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("KEGGREST")
+#X BiocManager::install("KEGG.db")
+#BiocManager::install("BioCor")
+#BiocManager::install("STRINGdb")
+#X library(pathifier)
+#X install.packages("neo4jshell")
 
-#source("nsga2.r")
+source("nsga2.r")
 source("matrices_evaluation.r")
 source("data_sampling.r")
+source("globals.r")
+source("evaluator.r")
 
-testFile = paste(sampleDir, "/Leukemia_GSE28497-3-col-sample-5-percent.csv", sep="")
-data <- readFile(testFile)
+test_file_name <- 'Leukemia_GSE28497-3-col-sample-5-percent.csv'
+test_file <- paste(sampleDir, "/", test_file_name, sep="")
+data <- readFile(test_file)
 
 datasets <- list(
   Leukemia = list(
@@ -53,9 +64,11 @@ datasets <- list(
 )
 
 data <- clean.and.translate.entrez.id(data, "GPL96")
+data <- data[ , order( as.numeric(colnames(data)) ) ]
 
-expressionMatrix = expression.matrix(t(data))
-biologicalMatrix = biological.matrix.go(colnames(data))
+gene_list <- colnames(data)
+dmatrix_expression = expression.matrix(t(data), dataset=test_file_name)
+dmatrix_biological = biological.matrix(gene_list, biological_databases$go, dataset=test_file_name)
 
 set.seed(1048)
-results <- nsga2.custom(expressionMatrix, biologicalMatrix)
+results <- nsga2.custom(dmatrix_expression, dmatrix_biological, ls_pos = 2, local_search = local_search_algorithms$pls, population_size = 20, generations = 10, neighborhood = 0.80)

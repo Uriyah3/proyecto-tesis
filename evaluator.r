fe39a35b558b7bab06426fbf8e6556c70954746f
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2021-04-13 19:22:41 -0400

    Add deferrable results runner

--- evaluator.r
+++ evaluator.r
@@ -1,6 +1,7 @@
 library(amap)
 library(mco)
 library(cluster)
+library(stringr)
 library(RDAVIDWebService)
 source("nsga2.r")
 
@@ -18,7 +19,7 @@ which.median = function(x) {
   }
 }
 
-evaluator.multiobjective.clustering <- function( results, dmatrix, debug = FALSE, which.x = which.max ) {
+evaluator.multiobjective.clustering <- function( results, dmatrix, debug = FALSE, which.x = which.max, dataset_name=NULL ) {
   
   silhouette_results <- evaluator.silhouette( results$clustering, dmatrix, debug)
   hypervolume_results <- evaluator.hypervolume( results$population, debug )
@@ -40,7 +41,29 @@ evaluator.multiobjective.clustering <- function( results, dmatrix, debug = FALSE
   return( metrics )
 }
 
-evaluator.multiobjective.clustering.no.bio <- function( results, dmatrix, debug = FALSE ) {
+evaluator.multiobjective.clustering.custom.bio <- function( results, dmatrix_expression, dataset_name, debug=FALSE, which.x = which.max ) {
+  bio_sources = list(
+    go = "gene ontology",
+    string = "STRING",
+    kegg = "KEGG-pathway",
+    disgenet_dis = "disgenet-disease"
+  )
+  
+  metrics <- evaluator.multiobjective.clustering.no.bio(results, dmatrix_expression, debug)
+  
+  gene_list <- colnames(dmatrix_expression)
+  biology_silhouette <- list()
+  for(source in bio_sources) {
+    dmatrix_bio <- biological.matrix(gene_list, source, dataset=dataset_name)
+    biology_silhouette[[source]] <- evaluator.silhouette(results$clustering, dmatrix_bio, debug)
+  }
+  dmatrix_bio <- NULL
+  
+  metrics$biological <- biology_silhouette
+  return(metrics)
+}
+
+evaluator.multiobjective.clustering.no.bio <- function( results, dmatrix, debug = FALSE, dataset_name=NULL ) {
   silhouette_results <- evaluator.silhouette( results$clustering, dmatrix, debug )
   hypervolume_results <- evaluator.hypervolume( results$population, debug )
   
@@ -117,7 +140,7 @@ evaluator.biological.anotate.list <- function( gene_list, debug = FALSE ) {
   # [INFO] Unable to sendViaPost to url[https://david.ncifcrf.gov/webservice/services/DAVIDWebService]
   # java.net.SocketTimeoutException: Read timed out
   # Considerar que también tira este error cuando la lista de genes es > 3000
-  setTimeOut(david, 150000)
+  setTimeOut(david, 300000)
   
   if (length(gene_list) >= 3000 || length(gene_list) <= 2) {
     if (debug) {
@@ -187,7 +210,7 @@ evaluator.biological.anotate.list <- function( gene_list, debug = FALSE ) {
   #)
 }
 
-evaluator.metaheuristics <- function(metaheuristic, meta_params, run_evaluator = evaluator.multiobjective.clustering.no.bio, runs = 13, debug = FALSE) {
+evaluator.metaheuristics <- function(metaheuristic, meta_params, run_evaluator = evaluator.multiobjective.clustering.no.bio, runs = 13, debug = FALSE, dataset_name = NULL) {
   
   results <- lapply( 1:runs, function(n) {
     if (debug) {
@@ -199,7 +222,7 @@ evaluator.metaheuristics <- function(metaheuristic, meta_params, run_evaluator =
     start.time <- Sys.time()
     
     metaheuristic_results <- do.call(metaheuristic, meta_params)
-    iteration_results = run_evaluator(metaheuristic_results, meta_params$dmatrix_expression, debug)
+    iteration_results = run_evaluator(metaheuristic_results, meta_params$dmatrix_expression, debug=debug, dataset_name=dataset_name)
     
     end.time <- Sys.time()
     time.taken <- end.time - start.time
@@ -219,6 +242,64 @@ evaluator.metaheuristics <- function(metaheuristic, meta_params, run_evaluator =
     full_results = full_results,
     mean_results = mean_results
   ))
+}
+
+save.metaheuristic.results <- function(dataset.name, identifier, metaheuristic, meta_params, runs = 13, debug = FALSE) {
+  lapply( 1:runs, function(n) {
+    if (debug) {
+      message("--------------------------------------------------")
+      message(paste("Running metaheuristic on iteration:", n))
+      message("\n\n\n")
+    }
+    
+    start.time <- Sys.time()
+    
+    iteration_results <- list()
+    iteration_results$nsga <- do.call(metaheuristic, meta_params)
+    
+    end.time <- Sys.time()
+    time.taken <- end.time - start.time
+    iteration_results$time <- time.taken
+    iteration_results$fitness_counter <- fitness_counter
+    
+    filename <- build.saved.results.filename(dataset.name, identifier, n)
+    saveRDS(iteration_results, filename)
+  })
+}
+
+reconstruct.metaheuristic.saved.results <- function(dataset.name, identifier, run_evaluator = evaluator.multiobjective.clustering.no.bio, runs = 13, debug = FALSE) {
+  dmatrix_expression <- expression.matrix(NULL, dataset=dataset.name)
+  results <- lapply( 1:runs, function(n) {
+    if (debug) {
+      message("--------------------------------------------------")
+      message(paste("Loading metaheuristic results on iteration:", n))
+      message("\n")
+    }
+    
+    filename <- build.saved.results.filename(dataset.name, identifier, n)
+    iteration_results <- readRDS(filename)
+    results = run_evaluator(iteration_results$nsga, dmatrix_expression, debug=debug, dataset_name=dataset.name)
+    iteration_results$nsga <- NULL
+    iteration_results <- c(results, iteration_results)
+    
+    iteration_results$silhouette$silhouette <- NULL
+    iteration_results
+  })
+  
+  full_results <- results
+  
+  mean_results <- unlist(results)
+  mean_results <- c(by(mean_results, names(mean_results), mean, na.rm = TRUE))
+  return( list(
+    full_results = full_results,
+    mean_results = mean_results
+  ))
+}
+  
+build.saved.results.filename <- function(dataset.name, identifier, iteration) {
+  return(
+    str_interp("cache/metaheuristic-${dataset.name}-${identifier}-${iteration}.rda")
+  )
 }
 
 #results <- evaluator.metaheuristics(nsga2.custom, list(dmatrix_expression=dmatrix_expression, dmatrix_biological=dmatrix_biological, population_size = 2, generations = 1, num_clusters = 3, ls_pos=NULL, local_search = NULL, debug=TRUE))
